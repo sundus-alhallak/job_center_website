5ce448a36f79dfcfe89d066db3e0a64769a1cf17
Sundus ALHALLAK <77814446+sundus-alhallak@users.noreply.github.com>
2021-06-12 01:56:01 +0300

    filtreleme

---
+++ #5 filtreleme.cs
@@ -0,0 +1,14 @@
+#5 filtreleme
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobSearch
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

bcf0b809e8403737d1e00c1beb8c7b3b2776ecf6
Sundus ALHALLAK <77814446+sundus-alhallak@users.noreply.github.com>
2021-06-12 02:36:07 +0300

    ilanlar

---
+++ #12 iş fırsatları kontrolu.cs
@@ -0,0 +1,145 @@
+#12 iş fırsatları kontrolu
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobSearch.Models;
+using System.IO;
+
+namespace JobSearch.Controllers
+{
+    public class JobsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            var jobs = db.Jobs.Include(j => j.categories);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name");
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,JobName,JobDiscription,JobImage,CategoryId")] Jobs jobs, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Path.Combine(Server.MapPath("~/Upload"), upload.FileName);
+                upload.SaveAs(path);
+                jobs.JobImage = upload.FileName;
+                db.Jobs.Add(jobs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", jobs.CategoryId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", jobs.CategoryId);
+            return View(jobs);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,JobName,JobDiscription,JobImage,CategoryId")] Jobs jobs, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                string OldPath = Path.Combine(Server.MapPath("~/Upload"), jobs.JobImage);
+                if (upload != null)
+                {
+                    System.IO.File.Delete(OldPath);
+                    string path = Path.Combine(Server.MapPath("~/Upload"), upload.FileName);
+                    upload.SaveAs(path);
+                    jobs.JobImage = upload.FileName;
+                    db.Entry(jobs).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                db.Entry(jobs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", jobs.CategoryId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jobs jobs = db.Jobs.Find(id);
+            db.Jobs.Remove(jobs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

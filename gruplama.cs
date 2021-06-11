f4178b592175537cfbb0f8e9d1ca0ebb5ad9e71c
Sundus ALHALLAK <77814446+sundus-alhallak@users.noreply.github.com>
2021-06-12 01:01:21 +0300

    kullanıcı grupları

---
+++ gruplama.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("grup ismi ")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("grupun açıklaması ")]
+        public string Discription { get; set; }
+
+        public virtual ICollection<Jobs> jobs { get; set; }
+    }
+}

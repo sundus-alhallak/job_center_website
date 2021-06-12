fdda970d0f4174988913bf1440ae76459f79760e
Sundus ALHALLAK <77814446+sundus-alhallak@users.noreply.github.com>
2021-06-12 08:58:06 +0300

    nitelik kümesi

---
+++ nitelikleri kümesi.cs
@@ -0,0 +1,24 @@
+ //#15 nitelikleri kümesi
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derleme hakkında genel bilgiler aşağıdakiler aracılığıyla kontrol edilir
+// nitelikler kümesi. Bilgileri değiştirmek için bu öznitelik değerlerini değiştirin
+// bir derleme ile ilişkili.
+[assembly: AssemblyTitle("JobSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JobSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92461a83-9509-425e-8686-4b665084e76e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
